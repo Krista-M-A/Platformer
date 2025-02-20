c4d6663fd08e7c2e7530dfaca5c85be772de215a
Krista-M-A <kmend943@gmail.com>
2025-02-19 22:56:02 -0800

    Probably as much as we're getting
    Haven't finished raycasting

--- Assets/Platformer/Scripts/GameManager.cs
+++ Assets/Platformer/Scripts/GameManager.cs
@@ -4,9 +4,47 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public TextMeshProUGUI timerText;
+    public TextMeshProUGUI coinText;
+    public TextMeshProUGUI pointText;
+
+    public Transform camera;
+    public float xPosition = 15.15f;
+
+    public int coinCount;
+    public int pointCount;
+
     void Update()
     {
         int timeLeft = 300 - (int)Time.time;
         timerText.text = $"Time: {timeLeft}";
+
+        if (coinCount < 10)
+        {
+            coinText.text = $"x0{coinCount}";
+        }
+        else
+        {
+            coinText.text = $"x{coinCount}";
+        }
+
+        pointText.text = $"{pointCount}";
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            if(xPosition > 192.15f)
+            {
+                xPosition = 192.15f;
+            }
+
+            camera.transform.position = new Vector3(++xPosition, 7.47f, -1f);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            if (xPosition < 15.15f)
+            {
+                xPosition = 15.15f;
+            }
+            camera.transform.position = new Vector3(--xPosition, 7.47f, -1f);
+        }
     }
 }
